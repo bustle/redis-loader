e6fcdc996033e7fd1566997f00ee34a6b5efb6f2
TSiege <tristan.t.siegel@gmail.com>
2018-04-02 09:29:35 -0400

    improve defaults for factory func

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as Redis from 'ioredis'
 import RedisLoader from './redis-loader'
 
-export default function redisLoader(redisUrl, options) {
+export default function redisLoader(redisUrl, options = {}) {
   const { logger, ...redisOptions } = options
-  const redis = new Redis(redisUrl, redisOptions || {})
+  const redis = new Redis(redisUrl, redisOptions)
 
   if (logger) {
     redis.on('error', err => logger(err))
