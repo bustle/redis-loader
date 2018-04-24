f22585b540bc69c820651056e53613c07de40650
Francis Gulotta <wizard@roborooter.com>
2018-04-24 12:31:05 -0400

    feat: lets go full typescript (#7)
    - Completely new stats interface with multiple inflight batch support
    - logger is now only called after a batch command to redis and it's only called with stats
    - errors will be returned on each dataloader's promise as necessary
    - multi failures will be returned on all the dataloader's promises and saved on the batchStats
    - target node 8 (es2017)
    - add travis
    - multi failures will be returned on all the dataloader's promises and saved on the batchStats 
    - api docs

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as Redis from 'ioredis'
+import { RedisLoader, statsLogger } from './redis-loader'
+export { RedisLoader }
+export interface RedisLoaderHelperOptions extends Redis.RedisOptions {
+  logger?: statsLogger
+}
+
+export default function redisLoader(redisUrl, options: RedisLoaderHelperOptions = {}) {
+  const { logger, ...redisOptions } = options
+  const redis = new Redis(redisUrl, redisOptions)
+  return new RedisLoader({ redis, logger })
+}
